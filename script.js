24c671458f1021a7577a93298149181ce6d61415
lxsilverfox <lucasxxavier315@gmail.com>
2024-06-01 23:30:43 -0300

    Att final
    Att final

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
+function isPalindrome(str) {
+    let converteLetrasParaMinusculo = str.toLowerCase();
+    let converteLetrasSemEspacos = converteLetrasParaMinusculo.replaceAll(' ', '')
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+    for (let i = 0; i < converteLetrasSemEspacos.length / 2; i++) {
+        if (converteLetrasSemEspacos[i] !== converteLetrasSemEspacos[converteLetrasSemEspacos.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 }
+isPalindrome("A SACaDa da cASa")
+
+
+function arrayMaxMin(arr) {
+    let menorNumero = 0;
+    let maiorNumero = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (menorNumero === 0) {
+            menorNumero = arr[i]
+            console.log(arr[i])
+        } else if (arr[i] < menorNumero) {
+            menorNumero = arr[i]
+        } if (arr[i] > maiorNumero) {
+            maiorNumero = arr[i]
+        }
+    }
+    return [menorNumero, maiorNumero];
+}
+arrayMaxMin([2, 30, 33, 11, 51, 56])
